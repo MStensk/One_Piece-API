3653e38ad8f782a2ebb41ba2265ddaf8caafd605
Mathias <Mathias_stensler@live.dk>
2025-12-14 23:56:07 +0100

    final commit. Code clean up would be nice, but i have been struggling with the droplet transfer. Hopefully everything works well when it is deployed.

--- src/main/java/org/SP2/Main.java
+++ src/main/java/org/SP2/Main.java
@@ -11,6 +11,7 @@ public class Main {
         ApplicationConfig
                 .getInstance()
                 .initiateServer()
+                .setCORS()
                 .setRoute(new Routes().all())
                 .setRoute(new SecurityRoutes().getSecurityRoute)
                 .startServer(7076);
